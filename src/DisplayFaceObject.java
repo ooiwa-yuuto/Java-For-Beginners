b0c81e380837d0b2f64b6e2df44b671f3b1e40e0
LAPTOP-CP979JV0\tibi2020 <zeal24460147@fsgcl.jp>
2025-06-02 12:13:30 +0900

    Faceの動きをmoveで追加する

--- src/DisplayFaceObject.java
+++ src/DisplayFaceObject.java
@@ -1,14 +1,18 @@
 
 public class DisplayFaceObject extends MyFrame {
 public void run() {
-	Face facel = new Face(50,50);
-	facel.draw(this);
+	Face facel = new Face(50,50,10,5);
 	
-	Face face2 = new Face(200,100);
-	face2.draw(this);
+	Face face2 = new Face(200,100,-10,-5);
 	
-	Face face3= new Face(150,75);
-	face3.draw(this);
-	
+	for(int i=0;i<30; i++) {
+		clear();
+		facel.draw(this);
+		face2.draw(this);
+		
+		facel.move();
+		face2.move();
+		sleep(0.1);
+	}
 }
 }
